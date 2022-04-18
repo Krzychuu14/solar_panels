62a45be15c49938eb8f81c6a35afa4c7ceea65d0
Krzychuu14 <biskrzysztof10@gmail.com>
2022-04-18 14:30:07 +0200

    Rysowanie po mapie
    Znalezienie strony do nauki mapy, oraz dodanie opcji rysowania na niej oraz zaznaczania punktów

---
+++ index.js
@@ -0,0 +1,33 @@
+function initMap() {
+        // Wyświetla mape na stronie
+    const map = new google.maps.Map(document.getElementById('map'), 
+    { zoom: 15, center: {lat: 50.021, lng: 19.938}, } );   // Nie wiem po co przecinek po 140.887}
+
+    const drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+                google.maps.drawing.OverlayType.MARKER,
+                google.maps.drawing.OverlayType.CIRCLE,
+                google.maps.drawing.OverlayType.POLYGON,
+                google.maps.drawing.OverlayType.POLYLINE,
+                google.maps.drawing.OverlayType.RECTANGLE,
+            ],
+        },
+        markerOptions: { icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png', },
+        circleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+        },
+    });
+
+    drawingManager.setMap(map);
+}
+
+window.initMap = initMap;   // Nie wiem o co chodzi
